c2e4048302845c8e10d3b6dbc0fa04c0aadd9bbc
AndersonPaschoalon <anderson.paschoalon@gmail.com>
2017-08-29 12:42:03 -0300

    up

---
+++ Python-Tests/traceAnalysis/run.py
@@ -0,0 +1,21 @@
+import os
+import time
+from GnuplotScripts.GnuplotScripts import GnuplotScripts
+from Cd.Cd import Cd
+
+PCAP_FILE = "../../Pcap/wireshark-wiki_http.pcap"
+SIMULATION_NAME = "wombat"
+
+
+def print_header(title):
+    print('')
+    print('###############################################################################')
+    print('# ' + title)
+    print('###############################################################################')
+
+def run_analyzis(pcap_file1, pcap_file2, analyzis_name, plots_dir):
+    print('TODO')
+
+def plot_data(pcap_file1, pcap_file2, analyzis_name, plots_dir):
+    print('TODO')
+
