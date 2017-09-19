33a16f05db9a20659cb491cc9fe3fbda1f6fd208
anderson <AndersonPaschoalon>
2017-09-19 15:26:49 -0300

    SimulationMininet run.py script added

--- Tests/SimulationMininet/run.py
+++ Tests/SimulationMininet/run.py
@@ -1,8 +1,40 @@
-# variaveis para definir: capture_name, duration(exec_time) interface ethernet(eth_if) e binario(bin_file)
-# 1. executar tcpdump na interface eth_if capturando pcap capture_name, e salvando no diretorio ./pcaps/
-# 2. executar binario do simitar bin_file
-# 3. depois de exec_time + 1s, matar o tcpdump
+import subprocess
+import time
+import os
+
+# execution options
+capture_name = "capture-name"
+ether_interface = "enp3s0"
+binary_command = "iperf-flow"  # from local directory, not blocking (using &)
+exec_time = 120
+
+# constants
+CAPTURE_DIR = './pcaps/'
 
 
+def print_header(title):
+    print('')
+    print('###############################################################################')
+    print('# ' + title)
+    print('###############################################################################')
 
 
+def main():
+    tcpdump_command = 'tcpdump -i ' + ether_interface + '-s 65535 -w ' + CAPTURE_DIR + capture_name + '.pcap'
+    os.system('mkdir -p ' + CAPTURE_DIR)
+
+    # capture pcap
+    print_header(' Tcpdump interface:' + ether_interface + ', pcap:' + capture_name)
+    proc_tcpdump = subprocess.Popen(tcpdump_command, shell=True)
+    # execute simitar app
+    print_header('Simitar-app command:' + binary_command)
+    os.system(binary_command + ' &')
+    # sleep until finishes
+    time.sleep(exec_time + 1)  # <-- There's no time.wait, but time.sleep.
+    # kill tcpdump
+    print_header('killing Tcpdump')
+    proc_tcpdump.kill()
+
+
+if __name__ == "__main__":
+    main()
