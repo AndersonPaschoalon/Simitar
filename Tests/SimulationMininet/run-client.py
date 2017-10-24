93e217812277443a49a2e186c3f26539401c97d3
anderson <AndersonPaschoalon>
2017-10-23 22:27:50 -0200

    now script is collecting rightly the pcap file

--- Tests/SimulationMininet/run-client.py
+++ Tests/SimulationMininet/run-client.py
@@ -3,7 +3,7 @@
 import subprocess
 import time
 import os
-from config import *
+from config import config
 
 def print_header(title):
 	print('')
@@ -13,26 +13,53 @@ def print_header(title):
 
 
 def main():
-	tcpdump_command = 'tcpdump -i ' + ether_interface_client + '-s 65535 -w ' + CAPTURE_DIR + capture_name + '.client.pcap'
-	os.system('mkdir -p ' + CAPTURE_DIR)
 
 	# capture pcap
-	print_header(' Tcpdump interface:' + ether_interface_client + ', pcap:' + capture_name)
-	#proc_tcpdump = subprocess.Popen(tcpdump_command, shell=True)
+	tcpdump_command = "tcpdump -i " + config.ether_interface_client + " -s 65535 -w " + config.capture_dir + config.pcap_name + ".client.pcap"
+	os.system('mkdir -p ' + config.capture_dir)
+	print_header("Tcpdump")
+	print("> interface: " + config.ether_interface_client)
+	print("> pcap file: " +config.pcap_name)
+	print("> tcpdump command: " + tcpdump_command)
 	os.system(tcpdump_command + " &")
-
+	
 	# client execution
-	print_header('Simitar-app command:' + binary_command_client + ", Mode:server")
-	os.system(binary_command_client + " &")	
+	print_header("simitar-gen, mode:client")
+	cd_dir = " cd " + config.simitargen_app_dir + "; "
+	script_client = cd_dir + " sudo -E ./simitar-gen --tool " + config.tool_name + " --mode client --ether " + config.ether_interface_client + " --xml " + config.ctd_xml_file  + " ";
+	print("> Execution mode: client")
+	if(config.tool_name == "iperf" or config.tool_name == "ditg"):
+		print("> Tool name: " + config.tool_name)
+		if(config.toplology_type == "tree"):
+			print("> Toplology type: " + config.toplology_type)
+			script_client = script_client + " --dst-list-ip " + config.dst_ip_file
+		elif (config.toplology_type == "simple"):
+			print("> Toplology type: " + config.toplology_type)
+			script_client = script_client + " --dst-ip " + config.dst_ip_addr
+		else:
+			print("> Invalid topology: " + config.toplology_type)
+			exit()
+	elif(config.tool_name == "tins"):
+		print("> Tool name: " + config.tool_name)
+		print("> Toplology type: " + config.toplology_type)
+		# nothing to configure
+	else:
+		print("> Invalid tool name: " + config.tool_name)
+		exit()
+	print("> Script client:" + script_client)
+	os.system(script_client + " &")	
 
 	# sleep until finishes
-	time.sleep(exec_time + 1)  # <-- There's no time.wait, but time.sleep.
+	time.sleep(config.exec_time + 1)  # <-- There's no time.wait, but time.sleep.
 
 	# kill tcpdump
-	print_header('killing Tcpdump')
-	#proc_tcpdump.kill()
-	os.system("killall tcpdump -9")
-
+	print_header('killing processes')
+	kill_tcpdump_command = "killall tcpdump -9"
+	kill_simitargen_command = "killall simitar-gen -9"
+	print("> " + kill_tcpdump_command)
+	os.system(kill_tcpdump_command)
+	print("> " + kill_simitargen_command)
+	os.system(kill_simitargen_command)
 
 if __name__ == "__main__":
     main()
--- Tests/SimulationMininet/run-server.py
+++ Tests/SimulationMininet/run-server.py
@@ -15,16 +15,29 @@ def print_header(title):
 
 
 def main():
-	tcpdump_command = 'tcpdump -i ' + ether_interface_server + '-s 65535 -w ' + CAPTURE_DIR + capture_name + '.server.pcap'
-	os.system('mkdir -p ' + CAPTURE_DIR)
-
 	# capture pcap
-	print_header(' Tcpdump interface:' + ether_interface_server + ', pcap:' + capture_name)
-	proc_tcpdump = subprocess.Popen(tcpdump_command, shell=True)
-	# execute simitar app
-	print_header('Simitar-app command:' + binary_command_server + ", Mode:server")
-	print("Press ctrl-C to finish")
-	os.system(binary_command_server + ' &')
+	tcpdump_command = 'tcpdump -i ' + config.ether_interface_server  + ' -s 65535 -w ' + config.capture_dir + config.pcap_name + '.server.pcap'
+	os.system('mkdir -p ' + config.capture_dir)
+	print_header("Tcpdump")
+	print("> interface: " + config.ether_interface_server)
+	print("> pcap file: " + config.pcap_name)
+	print("> tcpdump command: " + tcpdump_command)
+	os.system(tcpdump_command + " &")
+	#proc_tcpdump = subprocess.Popen(tcpdump_command, shell=True)
+
+	# server execution
+	print_header("simitar-gen, mode:server")
+	cd_dir = " cd " + config.simitargen_app_dir + "; "
+	script_server = cd_dir + " sudo -E ./simitar-gen --tool " + config.tool_name + " --mode server --ether " + config.ether_interface_server + " --xml " + config.ctd_xml_file;
+	print("> script server: " + script_server)
+	if( config.tool_name != "iperf" and config.tool_name != "ditg" and config.tool_name != "tins"):
+		print("> Invalid tool name: " + config.tool_name)
+		exit()
+	if(config.toplology_type != "tree" and config.toplology_type != "simple"):
+		print("> Invalid topology: " + config.toplology_type)
+		exit()
+	print("\nPress ctrl-C to finish")
+	os.system(script_server + ' &')
 	
 
 if __name__ == "__main__":
