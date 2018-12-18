2ab1a8573abee3ce24f04df9ae49e4749f872f35
anderson <anderson.paschoalon@gmail.com>
2018-12-17 23:41:41 -0200

    removed the third party code, now it is all on tar files

--- SIMITAR/deps/iperf-2.0.9/include/iperf-int.h
+++
@@ -1,9 +0,0 @@
-#ifndef _IPERF_INCLUDE_IPERF_INT_H
-#define _IPERF_INCLUDE_IPERF_INT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "iperf 2.0.9"
-/* generated using gnu compiler gcc (Ubuntu 5.4.0-6ubuntu1~16.04.10) 5.4.0 20160609 */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
